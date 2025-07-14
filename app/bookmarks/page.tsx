f1670361a8b9b4cee3edf767ab28bd213a775bd4
agwebryanmuna <agwebryan1@gmail.com>
2025-07-14 03:40:02 +0100

    Added auth with auth0

---
+++ app/bookmarks/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const BookmarksPage = () => {
+  return (
+    <div>BookmarksPage</div>
+  )
+}
+export default BookmarksPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-
-
 export default function Home() {
   return (
-    <div><h1>Pokemon App</h1></div>
+    <main>
+     Home
+    </main>
   );
 }
